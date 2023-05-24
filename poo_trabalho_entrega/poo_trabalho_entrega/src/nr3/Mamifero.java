63520ba3a55a353cc06e335aa760cd91f3440647
EdnilsonErnestoCuamba <133214827+EdnilsonErnestoCuamba@users.noreply.github.com>
2023-05-24 13:05:03 -0700

    Delete poo_trabalho_entrega directory

--- poo_trabalho_entrega/poo_trabalho_entrega/src/nr3/Mamifero.java
+++
@@ -1,8 +0,0 @@
-package nr3;
-
-public class Mamifero extends Animal {
-
-    public Mamifero(String nome, int nrPatas, String cor, String tipoLocomocao, char sexo) {
-        super(nome, nrPatas, "Terra", cor, tipoLocomocao, sexo);
-    }
-}
